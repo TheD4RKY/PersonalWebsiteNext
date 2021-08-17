7d5aaa68244e265fa8841a4e67453ede27f83d64
darky <growlbox123g@gmail.com>
2021-08-17 21:55:06 +0200

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ export default function index() {
   return (
     <>
       <head>
-        <script>var __adobewebfontsappname__="dreamweaver"</script>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       </head>
