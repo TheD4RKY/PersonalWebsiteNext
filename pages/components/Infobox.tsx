0c1344287c58d858477464af634e17e617e5b422
darky <growlbox123g@gmail.com>
2021-08-17 22:04:23 +0200

    bug fix for deploying page

--- pages/components/Infobox.tsx
+++ pages/components/Infobox.tsx
@@ -6,22 +6,22 @@ return(
     <>
     <div className={Style.infoboxes}>
         <div className={Style.littleinfobox}>
-            <h1 className={Style.stats}>I'M WORKING WITH</h1>
+            <h1 className={Style.stats}>IM WORKING WITH</h1>
             <h4>ADOBE</h4>
             <p>Dreamweaver and Photoshop</p>
         </div>
         <div className={Style.littleinfobox}>
-            <h1 className={Style.stats}>I'M FULFILLING</h1>
+            <h1 className={Style.stats}>IM FULFILLING</h1>
             <h4>EXPECTATIONS</h4>
             <p>Either mine or customers</p>
         </div>
         <div className={Style.littleinfobox}>
-            <h1 className={Style.stats}>I'M SPEAKING IN</h1>
+            <h1 className={Style.stats}>IM SPEAKING IN</h1>
             <h4>PROGRAMMING LANGUAGES</h4>
             <p>HTML, CSS, JS, JQUERRY etc.</p>
         </div>
         <div className={Style.littleinfobox}>
-            <h1 className={Style.stats}>I'M THINKING VERY</h1>
+            <h1 className={Style.stats}>IM THINKING VERY</h1>
             <h4>COMPLEX</h4>
             <p>With functionality and systems</p>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import React from 'react'
 import styles from '../styles/index.module.css'
 import style from '../scss/indexquerry.module.css'
 import glitch from '../scss/glitch.module.css'
+import Script from 'next/script'
 
 export default function index() {
   return (
     <>
       <head>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <Script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></Script>
+        <Script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></Script>
       </head>
       <body className={style.body}>
         
