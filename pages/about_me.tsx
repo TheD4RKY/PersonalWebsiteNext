ce0c638fa884a0a1cf0d13ffc2b4c4e9dcb2cd32
darky <growlbox123g@gmail.com>
2021-08-22 13:05:56 +0200

    bugfix / httpequip

--- pages/about_me.tsx
+++ pages/about_me.tsx
@@ -10,7 +10,6 @@ export default function Nav({ }) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             </Head>
             <Navbar />
             <Heroheader />
