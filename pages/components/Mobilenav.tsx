c74f0b0b067ae88b870e276dcdaedcdabbf0e2bd
darky <growlbox123g@gmail.com>
2021-08-29 01:40:07 +0200

    Mobile nav update

--- pages/components/Mobilenav.tsx
+++ pages/components/Mobilenav.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import style from "/scss/mobilenav.module.css";
+import styles from "/scss/mobilenav.module.css";
+import {IoIosList} from 'react-icons/io'
 
 export default function Mobilenav(){
+
     return(
 <>
-            
-                <div className={style.menu}>
-                    <div className={style.title}>MENU</div>
-                    <ul className={style.nav}>
+
+                <div className={styles.menu}>
+                    
+                    <ul className={styles.nav}>
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Work</a></li>
                         <li><a href="#">Resources</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
+                <div className={styles.title}>menu</div>
                 </div>
             
 
