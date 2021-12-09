a6a0cfebe164eee20f95343bdb0a6262f1dbe822
Dennis Kalužík <47431577+TheD4RKY@users.noreply.github.com>
2021-12-09 08:42:22 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/index.module.css'
 import style from '../scss/indexquerry.module.css'
 import glitch from '../scss/glitch.module.css'
-import Script from 'next/script'
 
 export default function index() {
   return (
     <>
-    <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-    </Head>
           <video className={style.bgvideo} autoPlay muted loop >
           <source src={'/videos/glitch1.mp4'} type="video/mp4" />
         </video>
