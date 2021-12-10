9eddc3a7875e9339d95adb0a31b919da6f8077a1
Dennis Kalužík <47431577+TheD4RKY@users.noreply.github.com>
2021-12-10 08:29:16 +0100

    Update Footer.tsx

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Style from "/scss/footer.module.css"
-
+import Style from "/scss/footer.module.css";
+import Link from 'next/link';
 export default function Footer(){
 return(
 <>
         <section className={Style.footer_banner} id="contact">
             <p className={Style.light2}>FOR MORE INFO &amp; REQUESTS</p>
-            <a href="/contact_me" id="ahref" ><div className={Style.button} data-aos="zoom-out">CONTACT ME</div></a>
+            <Link href="/contact_me" id="ahref" ><div className={Style.button} data-aos="zoom-out">CONTACT ME</div></Link>
         </section>
 </>
 )
