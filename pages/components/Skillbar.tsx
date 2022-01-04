bfc117a5629d0fca695eb2440eb9612d03286556
Dennis Kalužík <47431577+TheD4RKY@users.noreply.github.com>
2022-01-04 10:23:24 +0100

    Update Skillbar.tsx

--- pages/components/Skillbar.tsx
+++ pages/components/Skillbar.tsx
@@ -6,13 +6,13 @@ export default function Skillbar(){
     <>
 <div className={Style.container2}>
     <h1 className={Style.textskillmobile}>My skill level</h1>
-              <div className={Style["bar"] + " " + Style["learning2"]} data-skill="TS"></div>
+              <div className={Style["bar"] + " " + Style["learning2"]} data-skill="C"></div>
               <div className={Style["bar"] + " " + Style["back"] + " " + Style["basic"]} data-skill="C++"></div>
-              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate"]} data-skill="React"></div>
-              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate2"]} data-skill="NEXT"></div>
-              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate3"]} data-skill="JS"></div>
-              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced"]} data-skill="SASS"></div>
-              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced2"]} data-skill="CSS3"></div>
+              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate"]} data-skill="PHP"></div>
+              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate2"]} data-skill="VanillaJS"></div>
+              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate3"]} data-skill="React/NEXTjs"></div>
+              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced"]} data-skill="SASS/CSS3"></div>
+              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced2"]} data-skill="JS/TS"></div>
         <div className={Style["bar"] + " " + Style["front"] + " " + Style["expert"] } data-skill="HTML5"></div>
 </div>
 </>
