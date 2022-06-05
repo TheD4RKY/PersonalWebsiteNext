e7c8f819b0b2a626671d369be90bfec97ec2bb05
darky <growlbox123g@gmail.com>
2022-06-05 23:35:43 +0200

    profession update

--- pages/components/Topsectionabout.tsx
+++ pages/components/Topsectionabout.tsx
@@ -5,7 +5,7 @@ export default function Heroheader(){
 <div className={Style.heroimage}>
     <section className={Style.hero}>
         <h2 className={Style.hero_header}>DENNIS <span className={Style.light}>KALUZIK</span></h2>
-        <p className={Style.tagline}>WEB BUILDING &amp; DESIGNING</p>
+        <p className={Style.tagline}>FULL-STACK JUNIOR DEVELOPER</p>
     </section>
     
 </div>
