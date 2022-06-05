d728691609cd76cc92bc65e44f05123dc68b0960
darky <growlbox123g@gmail.com>
2022-06-05 23:34:38 +0200

    skills update

--- pages/components/Skillbar.tsx
+++ pages/components/Skillbar.tsx
@@ -5,15 +5,15 @@ export default function Skillbar(){
   return (  
     <>
 <div className={Style.container2}>
-    <h1 className={Style.textskillmobile}>My skill level</h1>
-              <div className={Style["bar"] + " " + Style["learning2"]} data-skill="C"></div>
+    <h1 className={Style.textskillmobile}>Skills i posses and comparison of their level</h1>
+              <div className={Style["bar"] + " " + Style["learning2"]} data-skill="SQL"></div>
               <div className={Style["bar"] + " " + Style["back"] + " " + Style["basic"]} data-skill="C++"></div>
-              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate"]} data-skill="PHP"></div>
-              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate2"]} data-skill="VanillaJS"></div>
+              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate"]} data-skill="JSON"></div>
+              <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate2"]} data-skill="PHP"></div>
               <div className={Style["bar"] + " " + Style["back"] + " " + Style["intermediate3"]} data-skill="React/NEXTjs"></div>
-              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced"]} data-skill="SASS/CSS3"></div>
+              <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced"]} data-skill="TailwindCSS"></div>
               <div className={Style["bar"] + " " + Style["front"] + " " + Style["advanced2"]} data-skill="JS/TS"></div>
-        <div className={Style["bar"] + " " + Style["front"] + " " + Style["expert"] } data-skill="HTML5"></div>
+        <div className={Style["bar"] + " " + Style["front"] + " " + Style["expert"]} data-skill="SASS/CSS3"></div>
 </div>
 </>
 )
