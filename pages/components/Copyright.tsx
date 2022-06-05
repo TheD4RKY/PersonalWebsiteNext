41d8995158cc15ee7e28c3f95611ad4a4c86f35b
darky <growlbox123g@gmail.com>
2022-06-05 23:33:40 +0200

    relinking files

--- pages/components/Copyright.tsx
+++ pages/components/Copyright.tsx
@@ -3,7 +3,7 @@ import Style from "/scss/copyright.module.css";
 export default function Copyright(){
 return(
     <>
-        <div className={Style.copyright}>&copy;2021- <strong>Dennis KaluZIk</strong></div>
+        <div className={Style.copyright}>&copy;2022 - <strong>Dennis KaluZIk</strong></div>
 </>
 )
 
--- pages/contact_me.tsx
+++ pages/contact_me.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Heroheader from './components/Topsectioncontact';
+import Heroheader from './components/contactme/Topsectioncontact';
 import Mobilenav from './components/Mobilenav';
 import Navbar from './components/Navright';
 import Navsocial from './components/Navleft';
-import Fromcover from './components/formcover';
+import Fromcover from './components/contactme/formcover';
 import Grid from "/scss/flextopheader.module.css";
 export default function contact() {
     return (
