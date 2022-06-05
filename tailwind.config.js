dd9b77d09d66ba437b37f289c95b18d56642ceac
darky <growlbox123g@gmail.com>
2022-06-05 23:33:06 +0200

    import my premade config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-// tailwind.config.js
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/*module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screens: {
       'tablet': '640px',
@@ -22,6 +13,49 @@ module.exports = {
 
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
-  }
-}*/
+    extend: {
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+      scale: {
+        '95': '0.95',
+        
+      },
+      height: { 
+        '128': '32rem', 
+        '138': '34rem',
+        '256': '62rem',
+      },
+      width: {
+        '100': '28rem',
+        '128': '32rem',
+        '256': '62rem',
+        '200': '38rem',
+      },
+      colors: {
+        'fadedblue': '#40445c'
+
+      }
+    },
+  },
+  plugins: [],
+}
