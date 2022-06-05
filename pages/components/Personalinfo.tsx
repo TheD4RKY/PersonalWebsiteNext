5e972a0a12334e12c88b126ee8e3346c69ca9aeb
darky <growlbox123g@gmail.com>
2022-06-05 23:34:10 +0200

    css edits

--- pages/components/Personalinfo.tsx
+++ pages/components/Personalinfo.tsx
@@ -10,8 +10,14 @@ return(
                 <Image src="/images/about_me.jpeg" alt="first picture" className={Style.cards} data-aos="zoom-out" data-aos-delay="300" width="330px;"
                     height="300px;" />
                 
-                    <p className={Style.textp}> {"lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore."}</p>
-               
+                <p className={Style.textp}> {"I'm 19 years old. I graduated High School in IT field on Vocational "}
+                    {"High School Klobouky u Brna. I love to code, learn about things, drive in cars, spend time with friends, exploring nature and play video games."}
+                </p><br /><p className={Style.textp}>
+                {"I think I'm fast learner in general even if I have to learn something that"}
+                {"could be quite boring. Maybe I'm not perfect but who is? What i find"}
+                {"perfect about me is that I'll do my best to learn things I don't already know"}
+                {"and grow my potencial."}
+                </p>
             </article>
             
             <article className={Style.footer_column}>
@@ -19,8 +25,9 @@ return(
                 <Image src="/images/about_work.png" alt="second picture" className={Style.cards} data-aos="zoom-out" data-aos-delay="300" width="330px;"
 	height="300px;" />
                 
-                    <p className={Style.textp}>{"lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore."}</p>
-                
+                <p className={Style.textp}>{"Right now I'm doing smaller projects in NextJS framework built on React "}
+                    {"with Typescript superset (For example coding my personal website and E-shop as a contract which are published on my GitHub page"}</p>
+                    <br /><p className={Style.textp}>{"Right now I'm in search of stable job where i can improve my skills, get to now someone new, make living and idealy work from home so i have more time for gym (i really need it) or for myself"}</p>
             </article>
             
         </footer>
