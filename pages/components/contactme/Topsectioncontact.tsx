a42fcdc6676fb9152a13b7a3cc2a52fd86085567
darky <growlbox123g@gmail.com>
2022-06-05 23:35:17 +0200

    replocating files

--- pages/components/contactme/Topsectioncontact.tsx
+++ pages/components/contactme/Topsectioncontact.tsx
@@ -4,8 +4,8 @@ export default function Heroheader() {
     return (
             <div className={Style.heroimage}>
             <section className={Style.hero}>
-                <h2 className={Style.hero_header}>DENNIS <span className={Style.light}>KALUZIK</span></h2>
-                <p className={Style.tagline}>FULLSTACK DEVELOPER</p>
+                <h2 className={Style.hero_header}>Contact me <span className={Style.light}>or Fill the form</span></h2>
+                <p className={Style.tagline}>Dennis Kalužík</p>
             </section>
         </div>   
     );
